7e9eb52dd417920b1ddf6c8f118f4f0fdafd9d0d
Aritra Das <aritrarick2002@gmail.com>
2023-01-24 21:39:43 +0530

    Splash Screen Done ✅

---
+++ app/src/main/java/com/example/quizapptask/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.quizapptask
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1500)
+
+    }
+}
